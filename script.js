5779eae59c484acf6f9632aa7d2052c4fa287115
yugpunjabi <yugpunjabi14@gmail.com>
2025-07-18 16:28:17 +0530

    Initial commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let result = document.getElementById("result");
 //Function to fetch data from API
 let getMovie = () => {
   let movieName = movieNameRef.value;
-  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
   //If input field is empty
   if (movieName.length <= 0) {
     result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
